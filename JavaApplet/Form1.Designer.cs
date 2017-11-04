54648979f4bb3ef73878e057d6f4b3a07d0f9ca5
Artur <artur.060969@gmail.com>
2017-11-04 02:53:35 +0000

    [CODE REWRITE] Colours fixed and added zoom in method.
    Implementation of zoom out has to be completed refactored as only working on form itself not paint.

--- JavaApplet/Form1.Designer.cs
+++ JavaApplet/Form1.Designer.cs
@@ -28,37 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(884, 503);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 503);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- JavaApplet/Form1.cs
+++ JavaApplet/Form1.cs
@@ -5,8 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace JavaApplet
 {
@@ -15,106 +15,169 @@ namespace JavaApplet
         public Form1()
         {
             InitializeComponent();
+            init();
             finished = false;
             x1 = 640;
             y1 = 480;
-            
+
             xy = (float)x1 / (float)y1;
             finished = true;
         }
-        class HSB
-        {//djm added, it makes it simpler to have this code in here than in the C#
-            public float rChan, gChan, bChan;
-            public HSB()
+        public struct HSBColor /* To set the colours */
+        {
+            float h;
+            float s;
+            float b;
+            int a;
+
+            public HSBColor(float h, float s, float b)
             {
-                rChan = gChan = bChan = 0;
+                this.a = 0xff;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
             }
-            public void fromHSB(float h, float s, float b)
+
+            public HSBColor(int a, float h, float s, float b)
             {
-                float red = b;
-                float green = b;
-                float blue = b;
-                if (s != 0)
+                this.a = a;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+            public float H
+            {
+                get { return h; }
+            }
+            public float S
+            {
+                get { return s; }
+            }
+            public float B
+            {
+                get { return b; }
+            }
+            public int A
+            {
+                get { return a; }
+            }
+            public Color Color
+            {
+                get
                 {
-                    float max = b;
-                    float dif = b * s / 255f;
-                    float min = b - dif;
-
-                    float h2 = h * 360f / 255f;
-
-                    if (h2 < 60f)
+                    return FromHSB(this);
+                }
+            }
+            public static Color FromHSB(HSBColor hsbColor)
+            {
+                float r = hsbColor.b;
+                float g = hsbColor.b;
+                float b = hsbColor.b;
+                if (hsbColor.s != 0)
+                {
+                    float max = hsbColor.b;
+                    float dif = hsbColor.b * hsbColor.s / 255f;
+                    float min = hsbColor.b - dif;
+                    float h = hsbColor.h * 360f / 255f;
+                    if (h < 60f)
                     {
-                        red = max;
-                        green = h2 * dif / 60f + min;
-                        blue = min;
+                        r = max;
+                        g = h * dif / 60f + min;
+                        b = min;
                     }
-                    else if (h2 < 120f)
+                    else if (h < 120f)
                     {
-                        red = -(h2 - 120f) * dif / 60f + min;
-                        green = max;
-                        blue = min;
+                        r = -(h - 120f) * dif / 60f + min;
+                        g = max;
+                        b = min;
                     }
-                    else if (h2 < 180f)
+                    else if (h < 180f)
                     {
-                        red = min;
-                        green = max;
-                        blue = (h2 - 120f) * dif / 60f + min;
+                        r = min;
+                        g = max;
+                        b = (h - 120f) * dif / 60f + min;
                     }
-                    else if (h2 < 240f)
+                    else if (h < 240f)
                     {
-                        red = min;
-                        green = -(h2 - 240f) * dif / 60f + min;
-                        blue = max;
+                        r = min;
+                        g = -(h - 240f) * dif / 60f + min;
+                        b = max;
                     }
-                    else if (h2 < 300f)
+                    else if (h < 300f)
                     {
-                        red = (h2 - 240f) * dif / 60f + min;
-                        green = min;
-                        blue = max;
+                        r = (h - 240f) * dif / 60f + min;
+                        g = min;
+                        b = max;
                     }
-                    else if (h2 <= 360f)
+                    else if (h <= 360f)
                     {
-                        red = max;
-                        green = min;
-                        blue = -(h2 - 360f) * dif / 60 + min;
+                        r = max;
+                        g = min;
+                        b = -(h - 360f) * dif / 60 + min;
                     }
                     else
                     {
-                        red = 0;
-                        green = 0;
-                        blue = 0;
+                        r = 0;
+                        g = 0;
+                        b = 0;
                     }
                 }
-
-                rChan = (float)Math.Round(Math.Min(Math.Max(red, 0f), 255));
-                gChan = (float)Math.Round(Math.Min(Math.Max(green, 0), 255));
-                bChan = (float)Math.Round(Math.Min(Math.Max(blue, 0), 255));
-
+                return Color.FromArgb
+                    (
+                        hsbColor.a,
+                        (int)Math.Round(Math.Min(Math.Max(r, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(g, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(b, 0), 255))
+                    );
             }
         }
-
-
-
-        const int MAX = 256;      // max iterations
-        const double SX = -2.025; // start value real
-        const double SY = -1.125; // start value imaginary
-        const double EX = 0.6;    // end value real
-        const double EY = 1.125;  // end value imaginary
+        private const int MAX = 256;
+        private const double SX = -2.025;
+        private const double SY = -1.125;
+        private const double EX = 0.6;
+        private const double EY = 1.125;
         private static int x1, y1, xs, ys, xe, ye;
         private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static bool action, rectangle, finished;
         private static float xy;
-        private Image picture;
         private Graphics g1;
+        private HSBColor HSBcol = new HSBColor();
+        private System.Drawing.Bitmap bitmap;
 
-        
+        public void init() // all instances will be prepared
+        {
 
-        private Cursor c1, c2;
-        private HSB HSBcol = new HSB();
-        private Pen pen;
-        Bitmap bm = new Bitmap(640, 480);
-
+            this.Size = new Size(640, 480);
+            finished = false;
+            x1 = 640;
+            y1 = 480;
+            xy = (float)x1 / (float)y1;
+            bitmap = new Bitmap(x1, y1);
+            g1 = Graphics.FromImage(bitmap);
+            finished = true;
+            start();
+        }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void destroy() // delete all instances 
+        {
+            if (finished)
+            {
+                bitmap = null;
+                g1 = null;
+                GC.Collect();
+            }
+        }
+        public void start()
         {
             action = false;
             rectangle = false;
@@ -122,74 +185,47 @@ namespace JavaApplet
             xzoom = (xende - xstart) / (double)x1;
             yzoom = (yende - ystart) / (double)y1;
             mandelbrot();
-         
         }
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        public void stop()
         {
-           
+        }
+
+        public void paint(Graphics g1)
+        {
+            update(g1);
         }
         public void update(Graphics g1)
         {
-            g1.DrawImage(picture, 0, 0);
-            if (rectangle)
-            {
-                if (xs < xe)
-                {
-                    if (ys < ye) g1.DrawRectangle(pen, xs, xe, (xe - xs), (ye - ys));
-                    else g1.DrawRectangle(pen, xs, ye, (xe - xs), (ys - ye));
-                }
-            }
-            else
-            {
-                if (ys < ye) g1.DrawRectangle(pen, xe, ys, (xs - xe), (ye - ys));
-                else g1.DrawRectangle(pen, xe, ye, (xs - xe), (ys - ye));
-            }
 
         }
         private void mandelbrot() // calculate all points
         {
             int x, y;
             float h, b, alt = 0.0f;
-
             action = false;
-            Console.WriteLine(x1 + " " + y1);
+            Color color = Color.Black;
+            Pen myPen = new Pen(color);
             for (x = 0; x < x1; x += 2)
                 for (y = 0; y < y1; y++)
                 {
-                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y);
                     if (h != alt)
                     {
-                        b = 1.0f - h * h; // brightnes
-                                          ///djm added
-                                          ///HSBcol.fromHSB(h,0.8f,b); //convert hsb to rgb then make a Java Color
-                                          ///Color col = new Color(0,HSBcol.rChan,HSBcol.gChan,HSBcol.bChan);
-                                          ///g1.setColor(col);
-                        //djm end
-                        //djm added to convert to RGB from HSB
-                        //djm test https://msdn.microsoft.com/en-us/library/microsoft.visualstudio.modeling.diagrams.hslcolor.aspx and use ToRGBColor
-                        HSBcol.fromHSB(h, 0.8f, b);
-                        int red = (int)(HSBcol.rChan * 255);
-                        int green = (int)(HSBcol.gChan * 255);
-                        int blue = (int)(HSBcol.bChan * 255);
-                        Color color = Color.FromArgb(red, green, blue);
-                        bm.SetPixel(x, y, color);
-                        bm.SetPixel(x + 1, y, color);
-                        //djm 
+                        b = 1.0f - h * h; //brightness
+                        
+                        color = HSBColor.FromHSB(new HSBColor(h * 255, 0.8f * 255, b * 255));
+                        
+                        myPen = new Pen(color);
                         alt = h;
                     }
-
+                    g1.DrawLine(myPen, x, y, x + 1, y);
                 }
-            pictureBox1.Image = bm;
-            action = true;
+                action = true;
         }
-        private static bool action, rectangle, finished;
-       
-       
         private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
         {
             double r = 0.0, i = 0.0, m = 0.0;
             int j = 0;
-
             while ((j < MAX) && (m < 4.0))
             {
                 j++;
@@ -208,6 +244,70 @@ namespace JavaApplet
             if ((float)((xende - xstart) / (yende - ystart)) != xy)
                 xstart = xende - (yende - ystart) * (double)xy;
         }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g1 = e.Graphics;
+            g1.DrawImage(bitmap, 0, 0, x1, y1);
+            g1.Dispose();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+                rectangle = true;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (action)
+            {
+                xs = e.X;
+                ys = e.Y;
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int z, w;
+        
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs > xe)
+                {
+                    z = xs;
+                    xs = xe;
+                    xe = z;
+                }
+                if (ys > ye)
+                {
+                    z = ys;
+                    ys = ye;
+                    ye = z;
+                }
+                w = (xe - xs);
+                z = (ye - ys);
+                if ((w < 2) && (z < 2)) initvalues();
+                else
+                {
+                    if (((float)w > (float)z * xy)) ye = (int)((float)ys + (float)w / xy);
+                    else xe = (int)((float)xs + (float)z * xy);
+                    xende = xstart + xzoom * (double)xe;
+                    yende = ystart + yzoom * (double)ye;
+                    xstart += xzoom * (double)xs;
+                    ystart += yzoom * (double)ys;
+                }
+                xzoom = (xende - xstart) / (double)x1;
+                yzoom = (yende - ystart) / (double)y1;
+                mandelbrot();
+                rectangle = false;
+                Refresh();
+            }
+        }
+
+
 
     }
 }
